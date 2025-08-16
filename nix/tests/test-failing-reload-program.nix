e48de04b718b06b489d019f03f6e74ff8297e6c0
Luflosi <luflosi@luflosi.de>
2025-08-16 22:40:53 +0200

    Extend test for a failing reload command
    Check what happens to the serial number after the failure.

--- nix/tests/test-failing-reload-program.nix
+++ nix/tests/test-failing-reload-program.nix
@@ -13,28 +13,48 @@
 let
   base = callPackage ./base.nix { };
 
-  config = lib.recursiveUpdate base.config {
+  config-failing = lib.recursiveUpdate base.config {
     reload_program_bin = "false";
     zones."example.org" = {
       reload_program_args = [ ];
+      soa.expire = "1001h";
     };
   };
+  config-working = base.config;
 
-  config-file = (formats.toml { }).generate "config-failing-reload-program.toml" config;
+  config-file-failing = (formats.toml { }).generate "config-failing-reload-program.toml" config-failing;
+  config-file-working = (formats.toml { }).generate "config-working-reload-program.toml" config-working;
 
-  expected-zone = base.generate-zone "example.org" config.zones."example.org" 1;
+  expected-zone-failing = base.generate-zone "example.org" config-failing.zones."example.org" 2;
+  expected-zone-working = base.generate-zone "example.org" config-working.zones."example.org" 1;
 in
   runCommand "zonewatch-test-failing-reload-program" { } ''
-    mkdir --verbose db
+    cp --verbose --no-preserve=mode -r '${base.state-after-initial-run}' 'after-initial-run'
+    cd 'after-initial-run'
     export RUST_LOG=zonewatch=trace
+
     echo 'Calling zonewatch is expected to fail:'
-    ! '${lib.getExe zonewatch-minimal}' --only-init --config '${config-file}'
+    ! '${lib.getExe zonewatch-minimal}' --only-init --config '${config-file-failing}'
     echo 'zonewatch failed as expected 🎉'
-    if ! diff '${expected-zone}' 'zones/example.org.zone'; then
+    if ! diff '${expected-zone-failing}' 'zones/example.org.zone'; then
       echo 'The zone file is different from what was expected!'
       exit 1
     fi
     echo 'The zone file is exactly what we expected 🎉'
+
+    echo 'Calling zonewatch again with a different config file is expected to work:'
+    '${lib.getExe zonewatch-minimal}' --only-init --config '${config-file-working}'
+    echo 'zonewatch failed as expected 🎉'
+    if ! diff '${expected-zone-working}' 'zones/example.org.zone'; then
+      echo 'The zone file is different from what was expected!'
+      exit 1
+    fi
+    echo 'The zone file is exactly what we expected 🎉'
+
+    if [ ! -e flag ]; then
+      echo 'The update program was not called!'
+      exit 1
+    fi
 
     touch "$out"
   ''
